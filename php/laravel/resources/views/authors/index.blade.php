0f4e2362c90621e21fa87f58548e6338e46ae5f7
Levy Barbosa <marcio.levy20@gmail.com>
2021-11-14 01:26:29 -0300

    improvements to listing html structure

--- php/laravel/resources/views/authors/index.blade.php
+++ php/laravel/resources/views/authors/index.blade.php
@@ -10,19 +10,29 @@
 @section('content')
   <ul class="list-group">
     @foreach($authors as $author)
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <div class="ms-2 me-auto">
-          <b>{{$author->fullName()}}</b> <span class="text-secondary">- {{$author->books->count()}} book(s)</span>
-        </div>
-        <div class="btn-toolbar">
-          <a href="/authors/{{$author->id}}" class="btn btn-outline-primary"><i class="fas fa-eye"></i></a>
-          <a href="/authors/{{$author->id}}/edit" class="btn btn-outline-dark mx-2"><i class="fas fa-edit"></i></a>
-          <form action="/authors/{{$author->id}}" method="post" class="require-confirm mb-0" confirm="Do you want to delete the author [{{$author->id}}] {{$author->fullName()}}?">
-            @csrf @method('DELETE') <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
-          </form>
+      <li class="list-group-item">
+        <div class="row">
+          <div class="col-10 col-sm-9 col-lg-10">
+            <b>{{$author->fullName()}}</b> <span class="text-secondary">- {{$author->books->count()}} book(s)</span>
+          </div>
+          <div class="col-2 col-sm-3 col-lg-2 ps-sm-0 ps-lg-4">
+            <div class="row">
+              <div class="col-12 col-sm-4 p-1">
+                <a href="/authors/{{$author->id}}" class="btn btn-outline-primary"><i class="fas fa-eye"></i></a>
+              </div>
+              <div class="col-12 col-sm-4 p-1">
+                <a href="/authors/{{$author->id}}/edit" class="btn btn-outline-dark"><i class="fas fa-edit"></i></a>
+              </div>
+              <div class="col-12 col-sm-4 p-1">
+                <form action="/authors/{{$author->id}}" method="post" class="require-confirm mb-0" confirm="Do you want to delete the author [{{$author->id}}] {{$author->fullName()}}?">
+                  @csrf @method('DELETE') <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </li>
     @endforeach
-  <ul>
+    </ul>
   {{ $authors->onEachSide(2)->links('components.pagination') }}
 @endsection
--- php/laravel/resources/views/books/index.blade.php
+++ php/laravel/resources/views/books/index.blade.php
@@ -10,19 +10,29 @@
 @section('content')
   <ul class="list-group">
     @foreach($books as $book)
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <div class="ms-2 me-auto">
-          <b>{{$book->title}}</b>
-        </div>
-        <div class="btn-toolbar">
-          <a href="/books/{{$book->id}}" class="btn btn-outline-primary"><i class="fas fa-eye"></i></a>
-          <a href="/books/{{$book->id}}/edit" class="btn btn-outline-dark mx-2"><i class="fas fa-edit"></i></a>
-          <form action="/books/{{$book->id}}" method="post" class="require-confirm mb-0" confirm="Do you want to delete book [{{$book->id}}] {{$book->title}}?">
-            @csrf @method('DELETE') <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
-          </form>
+      <li class="list-group-item">
+        <div class="row">
+          <div class="col-10 col-sm-9 col-lg-10">
+            <b>{{$book->title}}</b>
+          </div>
+          <div class="col-2 col-sm-3 col-lg-2 ps-sm-0 ps-lg-4">
+            <div class="row">
+              <div class="col-12 col-sm-4 p-1">
+                <a href="/books/{{$book->id}}" class="btn btn-outline-primary"><i class="fas fa-eye"></i></a>
+              </div>
+              <div class="col-12 col-sm-4 p-1">
+                <a href="/books/{{$book->id}}/edit" class="btn btn-outline-dark"><i class="fas fa-edit"></i></a>
+              </div>
+              <div class="col-12 col-sm-4 p-1">
+                <form action="/books/{{$book->id}}" method="post" class="require-confirm mb-0" confirm="Do you want to delete the book [{{$book->id}}] {{$book->title}}?">
+                  @csrf @method('DELETE') <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </li>
     @endforeach
-  <ul>
+    </ul>
   {{ $books->onEachSide(2)->links('components.pagination') }}
 @endsection
